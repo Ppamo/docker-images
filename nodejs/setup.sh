63ca2302d5882c955fc746fbff93bf03d211e8cb
Ppamo <pablo@ppamo.cl>
2017-02-21 17:00:30 -0300

    master - Add devel and nodejs images

---
+++ nodejs/setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+IMAGENAME=ppamo.cl/nodejs
+IMAGETAG=latest
+IMAGEVERSION=v0.5
+
+# check if docker is running
+docker info > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+	echo "Cannot connect to the Docker daemon. Is the docker daemon running on this host?"
+	exit -1
+fi
+
+# check if the Dockerfile is in the folder
+if [ ! -f Dockerfile ]
+then
+	echo "Dockerfile is not present, please run the script from right folder"
+	exit -1
+fi
+
+# check if the docker image exists
+docker images | grep "$IMAGENAME" | grep "$IMAGETAG" > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+	# Download node
+	if [ ! -f ./node-v6.9.5-linux-x64.tar.xz ]; then
+		wget https://nodejs.org/dist/v6.9.5/node-v6.9.5-linux-x64.tar.xz
+	fi
+	# create the docker image
+	docker build -t $IMAGENAME:$IMAGEVERSION -t $IMAGENAME:$IMAGETAG ./
+	if [ $? -ne 0 ]
+	then
+		echo "docker build failed!"
+		exit -1
+	fi
+fi
+
+# run a container from $IMAGENAME image
+docker run -di --publish-all "$IMAGENAME:$IMAGETAG"
